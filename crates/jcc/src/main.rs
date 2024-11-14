1cf9e86a3dad0ce6afbd1dfaf0c3f946f691b84c
João Silveira <joao.freixial.silveira@gmail.com>
2024-11-14 00:14:27 +0000

    feat: lex single char

--- crates/jcc/src/main.rs
+++ crates/jcc/src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::{Context, Result};
+// use bumpalo::Bump;
 use clap::Parser;
 use jcc::lexer::Lexer;
 use source_file::{diagnostic::Diagnostic, SourceDb, SourceFile};
@@ -16,18 +17,24 @@ fn main() -> Result<()> {
     // Global data structures
     let mut db = SourceDb::new();
     let mut interner = StringInterner::default();
-    // let bump = Bumpalo::bump();
+    // let bump = Bump::new();
 
     // Add file to db
     db.add(SourceFile::new(args.path).context("Failed to read file")?);
     let file = db.files().last().context("Failed to store file in db")?;
 
-    // Compile
+    // Compile the file
     let lexer_result = Lexer::new(&file, &mut interner).lex();
     if !lexer_result.diagnostics.is_empty() {
         lexer_result.diagnostics.iter().try_for_each(|d| {
             let diag = Diagnostic::from(d.clone());
             diag.report(&file, &mut std::io::stderr())
+        })?;
+    }
+    if !lexer_result.tokens.is_empty() {
+        lexer_result.tokens.iter().try_for_each(|t| {
+            let diag = Diagnostic::note(t.span, "lexed token", format!("{:?}", t.kind));
+            diag.report(&file, &mut std::io::stdout())
         })?;
     }
 

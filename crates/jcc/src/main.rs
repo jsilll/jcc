1fa23d6257e0b909f03a7d333f7e1bd107dd88f9
João Silveira <joao.freixial.silveira@gmail.com>
2025-09-03 13:28:13 +0100

    chore: small cleanup

--- crates/jcc/src/main.rs
+++ crates/jcc/src/main.rs
@@ -1,7 +1,7 @@
 use jcc::{
     ast::{graphviz::AstGraphviz, parse::Parser},
     lower::LoweringPass,
-    profile::{Pass, Profiler},
+    profile::Profiler,
     sema::{control::ControlPass, resolve::ResolverPass, ty::TyperPass, SemaCtx, TypeDict},
     ssa::SSABuilder,
     tok::lex::{Lexer, LexerDiagnosticKind},
@@ -12,7 +12,6 @@ use jcc_ssa::{
     amd64::{emit::AMD64Emitter, fix::AMD64Fixer},
     interner::Interner,
     sourcemap::{self, SourceDb, SourceMap},
-    verify::SSAVerifier,
 };
 
 use anyhow::{Context, Result};
@@ -69,7 +68,7 @@ fn main() {
     }
 }
 
-fn try_main(args: &Args, profiler: &mut Profiler) -> Result<()> {
+fn try_main(args: &Args, profiler: &mut Profiler<Pass>) -> Result<()> {
     // Run the preprocessor with `gcc -E -P`
     let pp_path = args.path.with_extension("i");
     profiler.time(Pass::Preprocessor, || -> Result<()> {
@@ -178,11 +177,6 @@ fn try_main(args: &Args, profiler: &mut Profiler) -> Result<()> {
     if args.verbose {
         println!("{}", ssa);
     }
-    let r = SSAVerifier::new(&ssa).verify();
-    if !r.diagnostics.is_empty() {
-        // TODO: properly report ssa errors
-        return Err(anyhow::anyhow!("exiting due to ssa verifier errors"));
-    }
     if args.tacky {
         return Ok(());
     }
@@ -234,4 +228,39 @@ fn try_main(args: &Args, profiler: &mut Profiler) -> Result<()> {
     })?;
 
     Ok(())
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum Pass {
+    Preprocessor,
+    Lexer,
+    Parser,
+    Resolver,
+    Control,
+    Typer,
+    Lowering,
+    SSABuild,
+    AMD64Build,
+    AMD64Fixer,
+    AssemblyEmission,
+    AssemblerAndLinker,
+}
+
+impl std::fmt::Display for Pass {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Pass::Preprocessor => write!(f, "Preprocessor (gcc -E)"),
+            Pass::Lexer => write!(f, "Lexer"),
+            Pass::Parser => write!(f, "Parser"),
+            Pass::Resolver => write!(f, "Resolver"),
+            Pass::Control => write!(f, "Control Pass"),
+            Pass::Typer => write!(f, "Typer Pass"),
+            Pass::Lowering => write!(f, "Lowering Pass"),
+            Pass::SSABuild => write!(f, "SSA Build"),
+            Pass::AMD64Build => write!(f, "AMD64 Build"),
+            Pass::AMD64Fixer => write!(f, "AMD64 Fixer"),
+            Pass::AssemblyEmission => write!(f, "Assembly Emission"),
+            Pass::AssemblerAndLinker => write!(f, "Assembler & Linker (gcc)"),
+        }
+    }
 }
